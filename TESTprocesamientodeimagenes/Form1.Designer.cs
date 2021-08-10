3d0598e3b65f41116232c37d123ab83ba68d22c3
Jesus Mtz <jesusmtzrdz98@gmail.com>
2021-08-10 17:03:53 -0500

    vercam

---
+++ TESTprocesamientodeimagenes/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TESTprocesamientodeimagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(0, 62);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 568);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(260, 143);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(822, 442);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(251, 91);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(599, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Copperplate Gothic Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(867, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Camara";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 629);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TESTprocesamientodeimagenes/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace TESTprocesamientodeimagenes
+{
+    //public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
+
+    {
+
+        private bool haycams;
+        private FilterInfoCollection MisCams;
+        private VideoCaptureDevice miCam;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cargarcams();
+        }
+
+        public void Cargarcams()
+        {
+            MisCams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if(MisCams.Count > 0)
+            {
+                haycams = true;
+                for (int i = 0; i< MisCams.Count; i++)
+                    {comboBox1.Items.Add(MisCams[i].Name.ToString());}
+                comboBox1.Text = MisCams[0].Name.ToString();
+            }
+            else
+            {
+                haycams = false;
+            }
+        }
+           
+
+
+        public void CerrarCam()
+        {
+            if(miCam !=null && miCam.IsRunning)
+            {
+                miCam.SignalToStop();
+                miCam = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CerrarCam();
+            int i = comboBox1.SelectedIndex;
+            string NombreCam = MisCams[i].MonikerString;
+            miCam = new VideoCaptureDevice(NombreCam);
+            miCam.NewFrame += new NewFrameEventHandler(capture);
+            miCam.Start();
+        }
+
+
+        private void capture(object sender , NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+
+            //intento de filtro invertido XD
+            //Bitmap pic = (Bitmap)eventArgs.Frame.Clone();
+            //for (int y = 0; (y <= (pic.Height - 1)); y++)
+            //{
+            //    for (int x = 0; (x <= (pic.Width - 1)); x++)
+            //    {
+            //        Color inv = pic.GetPixel(x, y);
+            //        inv = Color.FromArgb(255, (255 - inv.R), (255 - inv.G), (255 - inv.B));
+            //        pic.SetPixel(x, y, inv);
+            //    }
+            //}
+
+            //fin de intento
+
+            //Conclusion: funciona pero es muy lenta la imagen que muestra, brinca muchos frames xD
+
+            pictureBox1.Image = Imagen;
+            //pictureBox1.Image = pic;
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CerrarCam();
+        }
+
+        
+    }
+}
